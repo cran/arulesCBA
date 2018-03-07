1da1bdc52c38965082b3fe11a1630c3729b7b988
Ian Johnson <ianjjohnson@icloud.com>
2018-03-07 15:46:03 +0000

    version 1.1.3

--- inst/doc/arulesCBA.R
+++ inst/doc/arulesCBA.R
@@ -7,66 +7,53 @@ library(arulesCBA)
 
 
 ###################################################
-### code chunk number 2: arulesCBA.rnw:142-144
+### code chunk number 2: arulesCBA.rnw:139-140
 ###################################################
 data(iris)
-iris.disc <- as.data.frame(lapply(iris[1:4], function(x) discretize(x, categories=9)))
 
 
 ###################################################
-### code chunk number 3: arulesCBA.rnw:149-150
+### code chunk number 3: arulesCBA.rnw:146-147
 ###################################################
-iris.disc$Species <- iris$Species
+classifier <- CBA(Species ~ ., iris, supp = 0.05, conf=0.9)
 
 
 ###################################################
-### code chunk number 4: arulesCBA.rnw:155-156
-###################################################
-head(iris.disc)
-
-
-###################################################
-### code chunk number 5: arulesCBA.rnw:162-163
-###################################################
-classifier <- CBA(Species ~ ., iris.disc, supp = 0.05, conf=0.9)
-
-
-###################################################
-### code chunk number 6: arulesCBA.rnw:170-171
+### code chunk number 4: arulesCBA.rnw:154-155
 ###################################################
 print(classifier)
 
 
 ###################################################
-### code chunk number 7: arulesCBA.rnw:179-180
+### code chunk number 5: arulesCBA.rnw:163-164
 ###################################################
 rules(classifier)
 
 
 ###################################################
-### code chunk number 8: arulesCBA.rnw:185-186
+### code chunk number 6: arulesCBA.rnw:169-170
 ###################################################
 inspect(rules(classifier))
 
 
 ###################################################
-### code chunk number 9: arulesCBA.rnw:192-193
+### code chunk number 7: arulesCBA.rnw:176-177
 ###################################################
-classes <- predict(classifier, iris.disc)
+classes <- predict(classifier, iris)
 
 
 ###################################################
-### code chunk number 10: arulesCBA.rnw:198-200
+### code chunk number 8: arulesCBA.rnw:182-184
 ###################################################
 head(classes)
 table(classes)
 
 
 ###################################################
-### code chunk number 11: arulesCBA.rnw:208-211
+### code chunk number 9: arulesCBA.rnw:192-195
 ###################################################
 library(gmodels)
-CrossTable(classes, iris.disc$Species,
+CrossTable(classes, iris$Species,
            prop.chisq = FALSE, prop.r = FALSE, prop.c = FALSE)
 
 
